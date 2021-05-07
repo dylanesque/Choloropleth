273ae8395bdeb9031bef1bc6f6aa491b7938ca08
Michael Caveney <dylanesque27@gmail.com>
2021-05-06 21:58:55 -0400

    Completed MVP

--- chart.js
+++ chart.js
@@ -16,13 +16,13 @@ async function drawChloropleth() {
   ).features;
 
   const colors = [
-    '#2C26CE',
-    '#524DCF',
-    '#726EDD',
-    '#9C99EB',
-    '#9794EB',
-    '#C1BEF7',
-    '#EEEDFE',
+    '#CBCAF6',
+    '#9D9AE6',
+    '#7572D3',
+    '#524DCE',
+    '#352FB8',
+    '#120D87',
+    '#06034E',
   ];
 
   // 2. Draw Chart
@@ -55,19 +55,14 @@ async function drawChloropleth() {
     .attr('d', d3.geoPath())
     .attr('class', 'county')
     .attr('fill', (datum) => {
-      let countyId = datum.id;
-      let county = eduDataset.find((c) => c.fips === countyId);
-      let eduPercent = county.bachelorsOrHigher;
-      return mapEducationToColor(eduPercent);
+      return mapEducationToColor(getEducation(datum));
     })
     .attr('data-fips', (datum) => {
-      let countyId = datum.id;
-      let county = eduDataset.find((c) => c.fips === countyId);
+      let county = getCounty(datum);
       return county.fips;
     })
     .attr('data-education', (datum) => {
-      let countyId = datum.id;
-      let county = eduDataset.find((c) => c.fips === countyId);
+      let county = getCounty(datum);
       return county.bachelorsOrHigher;
     })
     .on('mouseover', onMouseOver)
@@ -91,6 +86,15 @@ async function drawChloropleth() {
     }
   }
 
+  function getCounty(datum) {
+    let countyId = datum.id;
+    return eduDataset.find((c) => c.fips === countyId);
+  }
+
+  function getEducation(datum) {
+    let county = getCounty(datum);
+    return county.bachelorsOrHigher;
+  }
   // 4. Create Scales
 
   // 5. Create Axes
@@ -104,16 +108,16 @@ async function drawChloropleth() {
     .style('visibility', 'hidden');
 
   function onMouseOver(d) {
-    console.log(d);
+    let datum = d;
     tooltip.transition().duration(200).style('visibility', 'visible');
+    // Show state, county, and education
     tooltip
-      .html("testing"
-      )
       .style('left', d3.event.pageX + 'px')
       .style('top', d3.event.pageY - 28 + 'px')
-      .attr('data-year', d.year)
-      .attr('data-month', d.month - 1)
-      .attr('data-temp', varAccessor(d));
+      .attr('data-education', () => {
+        let county = getCounty(datum);
+        return county.bachelorsOrHigher;
+      });
   }
 
   function onMouseLeave() {
